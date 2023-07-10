78138da7c8782aff9fb128945541da086e790420
Nabirall <drozdover99@mail.ru>
2023-07-10 15:29:12 +0300

    var2

---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,32 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CarDaoImpl implements CarDao{
+
+private List<Car> carList;
+
+{
+    carList = new ArrayList<>();
+
+    carList.add(new Car("BMW", "X6", 2019));
+    carList.add(new Car("Mercedes", "500", 2011));
+    carList.add(new Car("Ferrari", "F8",2081));
+    carList.add(new Car("Audi", "A7",2048));
+    carList.add(new Car("Ford", "GT",2026));
+}
+
+
+
+@Override
+public List<Car> getCar(int count) {
+    return carList.stream().limit(count).collect(Collectors.toList());
+}
+
+}
